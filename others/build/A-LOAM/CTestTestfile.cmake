0e46016bd6da8234b6371cee4b3d9f15bb6a333c
Yang Shi <946738418@qq.com>
2019-05-25 00:26:34 +0800

    first push

--- others/build/A-LOAM/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shiyang/advanced_car/src/A-LOAM
-# Build directory: /home/shiyang/advanced_car/build/A-LOAM
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
